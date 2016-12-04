201b270616879a0886fa127f6d100ed4ba787d5e
Oleg <margin2call@mail.ru>
2016-12-04 13:04:12 +0200

    code style

--- src/components/todo-list.jsx
+++ src/components/todo-list.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import TodoListItem from './todo-list-item';
 
-const TodoList = ({todos}) =>
+const TodoList = ({ todos }) =>
         <ul className="todo-list">
           {todos.map((todo, index) => <TodoListItem name={todo.name} key={index}></TodoListItem>)}
         </ul>;
